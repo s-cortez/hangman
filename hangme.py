af0fc3c2fe547c333a6480a20ef2fea09e09e249
Saby Cortez <sabymong@gmail.com>
2018-09-28 15:06:46 -0500

    initial commit part 2

---
+++ hangme.py
@@ -0,0 +1,31 @@
+"""
+Hangman
+Author: Saby Cortez
+"""
+import turtle
+
+def get_word(min_len, max_len, text_file):
+    """
+    Returns a word for use in a hangman game
+    :param min_len: Minimum length of the word
+    :param max_len: Maximum length of the word
+    :param text_file: Name of the text file to pull from
+    :return: A word
+    """
+    raise RuntimeError("get_word not yet implemented")
+
+
+def checkInput():
+    ""
+    pass
+
+
+def drawPicture(turt1, turt2, turt3, turt4):
+    pass
+
+
+def startgame():
+    pass
+
+
+startgame()
