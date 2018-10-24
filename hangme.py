0f282bcd63db46fb353423e03a15a686c0dcc509
Saby Cortez <sabymong@gmail.com>
2018-10-24 00:24:58 -0500

    Cleaned up some code

--- hangme.py
+++ hangme.py
@@ -10,7 +10,6 @@ def chooseWord():
     Returns a random word from words/custom.txt
     :return: random word as a string
     """
-    trys = 10
     words = "words/custom.txt"
     file = open(words)
     wordlist = file.read().splitlines()
@@ -19,29 +18,47 @@ def chooseWord():
     return word
 
 
-def checkWord():
-    word = chooseWord()
+def updateDisplayWord(guess: chr, realWord: str, currentDisplayWord: str) -> str:
+    """
+    Fills in the blanks of a masked word given a guess
+    :param guess: the character to fill in
+    :param realWord: string containing the full word
+    :param currentDisplayWord: string containing all revealed guesses
+    :return: updated display word
+    """
+    return ""
+
+
+def main():
+    """
+    User interface for hangman game
+    :return: None
+    """
+
+    # Initialize variables
+    realword = chooseWord()
     trys = 7
-    used = ""
-    blank = "_ "
-    length = blank * len(word)
+    used = []
+    displayword = ("_ " * len(realword)).strip()
+
+    # Main game
     while trys > 0:
-        print(length)
+        print(displayword)
         print("You have", trys, "tries left")
         x = input("Pick a letter!")
         if len(x) > 1:
             print("Maybe just put 1 letter plz")
         if x in used:
             print("Error! You've already used that letter silly!")
-        elif x in word:
-            new = word.count(x)
+        elif x in realword:
+            new = realword.count(x)
             print("ok cool theres", new, " ", x)
             used += x
-            pos = word.find(x)
+            pos = realword.find(x)
             lengthpos = 2 * pos
-            b = list(length)
+            b = list(displayword)
             b[lengthpos] = x
-            length = "".join(b)
+            displayword = "".join(b)
 
 
         else:
@@ -49,5 +66,4 @@ def checkWord():
             print("oof sorry no ", x)
             used += x
         print("You've used: ", used)
-    print("The word was:", word)
-    return trys
+    print("The word was:", realword)
