f7bf75d43d907dc3a723c0542a1d50af1e3419f8
Saby Cortez <sabymong@gmail.com>
2018-10-24 00:24:39 -0500

    Wrote some tests

--- test_hangMe.py
+++ test_hangMe.py
@@ -2,11 +2,6 @@ from unittest import TestCase
 from hangme import *
 
 
-class TestCheckWord(TestCase):
-    def test_checkWord(self):
-        self.fail()
-
-
 class TestChooseWord(TestCase):
     def test_checkWordIsFromFile(self):
         words = "words/custom.txt"
@@ -14,3 +9,20 @@ class TestChooseWord(TestCase):
         wordlist = file.read().splitlines()
         file.close()
         self.assertTrue(chooseWord() in wordlist)
+
+
+class TestUpdateDisplayWord(TestCase):
+    def test_updatesWithBlankDisplayWord(self):
+        realWord = "cat"
+        displayWord = "_ _ _"
+        guess = "c"
+
+        self.assertEqual("c _ _",
+                         updateDisplayWord(guess, realWord, displayWord))
+
+    def test_updatesWithNonBlankDisplayWord(self):
+        self.fail()
+
+    def test_failsOnUnequalLengthWords(self):
+        # Fail if pass in "cat" with "_ _ _ _ _"
+        self.fail()
